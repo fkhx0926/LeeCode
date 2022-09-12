d4fdd964d00d41d7ff93952e7a951da4eed72458
shaoqiang <2428763987@qq.com>
2022-09-12 17:53:19 +0800

    push-commit

--- src/one/Picture/a1.java
+++ src/one/Picture/a1.java
@@ -4,6 +4,6 @@ public class a1 {
     public static void main(String[] args) {
         System.out.println("111");
         System.out.println("222");
-
+        System.out.println("push test");
     }
 }
