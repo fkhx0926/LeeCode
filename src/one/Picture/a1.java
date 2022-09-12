2cb798aae510bb5118b8331db77cf35f1928287b
XingHuo <78941910+fkhx0926@users.noreply.github.com>
2022-09-12 17:55:40 +0800

    111

--- src/one/Picture/a1.java
+++ src/one/Picture/a1.java
@@ -5,5 +5,6 @@ public class a1 {
         System.out.println("111");
         System.out.println("222");
         System.out.println("push test");
+           System.out.println("pull test");
     }
 }
